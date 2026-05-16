80cd36fec2ffc07234d1cdccd07f9cb0acc17b7a
Михаил <isataevmihail@gmail.com>
2026-05-17 01:06:17 +0500

    Fix github language statistics using gitattributes

---
+++ engine/target/debug/deps/fin_engine-2c7458dac16cd1ea.d
@@ -0,0 +1,9 @@
+C:\Users\Михаил\Desktop\fin-helper\engine\target\debug\deps\fin_engine-2c7458dac16cd1ea.d: fin-grpc\src\main.rs Cargo.toml
+
+C:\Users\Михаил\Desktop\fin-helper\engine\target\debug\deps\libfin_engine-2c7458dac16cd1ea.rmeta: fin-grpc\src\main.rs Cargo.toml
+
+fin-grpc\src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ engine/target/debug/deps/fin_engine-90f0d58192ac5338.d
@@ -0,0 +1,9 @@
+C:\Users\Михаил\Desktop\fin-helper\engine\target\debug\deps\fin_engine-90f0d58192ac5338.d: fin-grpc\src\main.rs Cargo.toml
+
+C:\Users\Михаил\Desktop\fin-helper\engine\target\debug\deps\libfin_engine-90f0d58192ac5338.rmeta: fin-grpc\src\main.rs Cargo.toml
+
+fin-grpc\src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
